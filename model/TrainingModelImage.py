01e516601a60f5bb62c52bcb0502df00e284a5bf
ChuTrungKien <chutrungkien11997@gmail.com>
2024-08-08 15:03:12 +0700

    kien commit debug flask

--- model/TrainingModelImage.py
+++ model/TrainingModelImage.py
@@ -1,2 +1,12 @@
-import numpy as np
-import cv2
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
